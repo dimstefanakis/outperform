96f82a0fb630efda836f73e9f0260ed9b730d787
Dimitris <jimstef@outlook.com>
2024-09-01 23:53:21 +0300

    css fix

--- components/shared/CaseStudy.tsx
+++ components/shared/CaseStudy.tsx
@@ -5,7 +5,7 @@ import { CaseStudy as CaseStudyInterface } from '@/types'
 export default function CaseStudy({ data }: { data: CaseStudyInterface }) {
   return (
     <div className="grid md:grid-cols-2 gap-4">
-      <div className="grid grid-cols-2">
+      <div className="flex flex-col">
         <div className="flex flex-col text-white mb-10">
           <h1 className="text-4xl whitespace-break-spaces">
             {data?.studyTitle}
@@ -14,20 +14,22 @@ export default function CaseStudy({ data }: { data: CaseStudyInterface }) {
             {data?.studyDescription}
           </span>
         </div>
-        {data?.studyPillars?.map((pillar, index) => (
-          <div
-            key={index}
-            className={`flex flex-col mb-6 col-span-2 md:col-span-${pillar.columnSpan}`}
-          >
-            <h3 className="text-sm font-bold text-white uppercase">
-              {pillar.title}
-            </h3>
-            <Separator className="w-[50px] my-4 bg-white" />
-            <p className="text-lg max-w-full md:max-w-[80%] ml-0 md:ml-6 text-gray-500">
-              {pillar.description}
-            </p>
-          </div>
-        ))}
+        <div className="grid grid-cols-2">
+          {data?.studyPillars?.map((pillar, index) => (
+            <div
+              key={index}
+              className={`flex flex-col mb-6 col-span-2 md:col-span-${pillar.columnSpan}`}
+            >
+              <h3 className="text-sm font-bold text-white uppercase">
+                {pillar.title}
+              </h3>
+              <Separator className="w-[50px] my-4 bg-white" />
+              <p className="text-lg max-w-full md:max-w-[80%] ml-0 md:ml-6 text-gray-500">
+                {pillar.description}
+              </p>
+            </div>
+          ))}
+        </div>
       </div>
       <div className="col-span-2 md:col-span-1 flex flex-col">
         <ImageBox
