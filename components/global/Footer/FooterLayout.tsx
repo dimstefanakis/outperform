362e2758898c285345f4bf1d9efb533071940ada
Dimitris <jimstef@outlook.com>
2024-09-11 16:39:55 +0900

    footer fixes

--- components/global/Footer/FooterLayout.tsx
+++ components/global/Footer/FooterLayout.tsx
@@ -4,6 +4,7 @@ import type { PortableTextBlock } from 'next-sanity'
 import { Separator } from '@/components/ui/separator'
 import { CustomPortableText } from '@/components//shared/CustomPortableText'
 import type { SettingsPayload, FooterPayload } from '@/types'
+import ImageBox from '@/components/shared/ImageBox'
 
 interface FooterProps {
   data: FooterPayload
@@ -12,13 +13,30 @@ export default function Footer(props: FooterProps) {
   const { data } = props
   const footer = data
   return (
-    <footer className="bottom-0 w-full flex justify-center items-center bg-white  text-center ">
-      <div className="w-full md:max-w-[95%] border-t border-gray-200 py-3 md:py-3 flex">
+    <footer className="bottom-0 w-full flex justify-center items-center bg-white text-center ">
+      <div className="w-full text-gray-500 max-w-[90%] border-t border-gray-200 py-3 md:py-3 flex">
         {footer && footer.leftItems && (
-          <div className="flex flex-col md:flex-row items-center justify-center">
+          <div className="flex flex-col md:flex-row items-start md:items-center justify-center">
             {footer.leftItems.map((item, index) => (
               <div key={index} className="mr-3">
                 <Link href={item.url || ''}>{item.text}</Link>
+              </div>
+            ))}
+          </div>
+        )}
+        {footer && footer.rightItems && (
+          <div className="flex flex-row justify-end flex-1">
+            {footer.rightItems.map((item, index) => (
+              <div key={index} className="mr-3">
+                <Link href={item.url ? item.url : ''}>
+                  <ImageBox
+                    image={item.logo}
+                    alt={item.text || ''}
+                    width={item.logo?.asset.metadata.dimensions.width}
+                    height={item.logo?.asset.metadata.dimensions.height}
+                    imageClasses="w-10 h-10"
+                  />
+                </Link>
               </div>
             ))}
           </div>
